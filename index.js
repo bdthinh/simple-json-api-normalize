f41e4d64f3a43d9a31cb0cbcc1203a1a2ebbd1b4
Thinh D. Bui <jinsherlock@gmail.com>
2020-04-23 16:11:14 +0800

    Feat: data array inside relationships

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 const getAttributes = record => record && record.attributes;
 const getRelationships = record => record && record.relationships;
 
+const polyfillFullData = (included = []) => compactData => {
+  const data =
+    included.find(inc => inc.type === compactData.type && inc.id === compactData.id) || compactData;
+
+  let polyfill = getAttributes(data);
+  if (getRelationships(data)) {
+    polyfill = {
+      ...polyfill,
+      ...polyfillAttributes(included)(data),
+    };
+  }
+
+  return {
+    id: data.id,
+    type: data.type,
+    ...polyfill,
+  };
+};
+
 const polyfillAttributes = (included = []) => record => {
   const relationships = getRelationships(record);
   if (!relationships) return [];
 
   return Object.keys(relationships).reduce((acc, key) => {
     const currentRelation = relationships[key];
-    const extendRelation =
-      included.find(
-        inc => inc.type === currentRelation.data.type && inc.id === currentRelation.data.id
-      ) || currentRelation;
 
-    if (!extendRelation) return acc;
+    const relationData = currentRelation.data;
+    if (!relationData) return acc;
 
-    let polyfill = getAttributes(extendRelation);
-    if (getRelationships(extendRelation)) {
-      polyfill = {
-        ...polyfill,
-        ...polyfillAttributes(included)(extendRelation),
-      };
+    let value = undefined;
+
+    if (Array.isArray(relationData)) {
+      value = relationData.map(eachRelation => polyfillFullData(included)(eachRelation));
+    } else {
+      value = polyfillFullData(included)(relationData);
     }
-
     return {
       ...acc,
-      [key]: {
-        id: extendRelation.id,
-        type: extendRelation.type,
-        ...polyfill,
-      },
+      [key]: value,
     };
   }, {});
 };
